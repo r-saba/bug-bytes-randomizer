ec251ea6ed948ac10e6476406abae44450cc5160
ragith <ragith.coolkid@gmail.com>
2020-12-13 18:45:24 -0500

    feat: add randomizer logic and styling

---
+++ randomizer.js
@@ -0,0 +1,18 @@
+function _randomizeNames() {
+    let names = ["Ragith", "Ritchelle", "Cindy", "Cristian", "Leo", "Yun Shi", "Jimmy", "Jonathan", "Omar"];
+    return names.sort(() => Math.random() - 0.5);
+}
+
+function makeList() {
+    randomNames = _randomizeNames();
+    listElement = document.createElement("ol");
+
+    randomNames.forEach(name => {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = name;
+        listElement.appendChild(listItem);
+    });
+    document.getElementsByClassName('name-list')[0].appendChild(listElement)
+}
+
+document.onload = makeList();
